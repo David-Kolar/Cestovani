44c7b8fd7c3c035fa9bcd1569fa42b6717cd071e
david <kolard@jirovcovka.net>
2022-05-31 19:05:39 +0200

    Initial Commit

--- main.py
+++ main.py
@@ -3,28 +3,55 @@ from random import randint, choice
 from collections import deque
 
 
-def save_graph(file_name):
-    pass
+def save_graph(file_name, number_of_nodes, edges):
+    with open(file_name, "w+") as file:
+        file.write(str(number_of_nodes))
+        for edge in edges:
+            edge = [str(i) for i in edge]
+            edge = "\n" + " ".join(edge)
+            file.write(edge)
 
-def make_random_tree(number_of_nodes, val_min, val_max):
+
+def make_random_tree(number_of_nodes, val_min=1, val_max=1):
     nodes_used = 0
     graph = [[] for node in range(number_of_nodes)]
+    edges = []
     for node in range(1, number_of_nodes):
         neighbour = randint(0, nodes_used)
         dist = randint(val_min, val_max)
         graph[neighbour].append((node, dist))
         graph[node].append((neighbour, dist))
+        edges.append((node, neighbour, dist))
+        edges.append((neighbour, node, dist))
         nodes_used += 1
-    return graph
+    return graph, number_of_nodes, edges
 
 
-def make_random_connected_graph(number_of_nodes, number_of_edges):
+def make_random_connected_graph(number_of_nodes, number_of_edges, val_min=1, val_max=1):
     if (number_of_nodes > number_of_edges + 1):
         raise ValueError("Number of edges must be at max 1 lower than number of nodes!")
-    graph = make_random_tree(number_of_nodes)
+    graph, n_tree, edges = make_random_tree(number_of_nodes)
     number_of_edges -= number_of_nodes - 1
     for _ in range(number_of_edges):
-        neighbour = randint()
+        while(True):
+            nodeA = randint(0, number_of_nodes - 1)
+            nodeB = randint(0, number_of_nodes - 1)
+            if (nodeB == nodeA):
+                continue
+            edge_exist = False
+            for node, dist in graph[nodeA]:
+                if (node == nodeB):
+                    edge_exist = True
+                    break
+            if not(edge_exist):
+                break
+
+        dist = randint(val_min, val_max)
+        graph[nodeA].append((nodeB, dist))
+        graph[nodeB].append((nodeA, dist))
+        edges.append((nodeA, nodeB, dist))
+        edges.append((nodeB, nodeA, dist))
+    return graph, number_of_nodes, edges
 
 
 
@@ -79,7 +106,6 @@ def vytvor_tabulky(graph):
 
 
 
-nodes, edges = load_graph("data/graf01")
+nodes, edges = load_graph("data/graf02")
 graph = make_graph(nodes, edges)
-print(vytvor_tabulky(graph))
 
